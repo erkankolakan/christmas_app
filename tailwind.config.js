b784cbb2fdce2641d9407e0b193ecadd1298c604
Erkan Kolakan <erkankolakan@gmail.com>
2024-01-01 18:14:57 +0300

    nativewind temel renkler

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./screens/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "kirmizi3":"#FF0000",
+        "kirmizi1":"#d40028",
+        "kirmizi2":"#c30022",
+        "yesil1":"#036f3e",
+        "yesil2":"#00a53c",
+        "yesil3":"#13cc4e",
+      }
+    },
   },
   plugins: [],
 }
